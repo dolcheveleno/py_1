51076874b14edaa1a2a0c150e1d7ab8ed40745e6
Olya <dolcheveleno@mail.ru>
2022-09-30 12:09:30 +0300

    4

--- 4.py
+++ 4.py
@@ -1,15 +1,13 @@
 # Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число
 
-import random
-N = int(input("Введите размер списка: ")) 
-a = []
-for i in range(N):  
-    new_element = random.randint(-N, N)
-    a.append(new_element)
-print(f'Ваш список: {a}')
-num = 1
-with open('file.txt') as file:
-    for pos in file:
-        if int(pos) < N:
-            num *= a[int(pos)]
-print(num)
+from random import randint
+n = 5
+lst = []
+result = 1
+for i in range(n):
+    lst.append(randint(n * -1, n))
+print(lst)
+with open('file.txt', 'r') as f:
+    for i in f:
+        result *= lst[int(i) - 1]
+    print(result)
