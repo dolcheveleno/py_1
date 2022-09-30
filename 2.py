da9e499d414b3360e78d1bc23c888d94e36dd581
Olya <dolcheveleno@mail.ru>
2022-09-30 11:53:28 +0300

    2

---
+++ 2.py
@@ -0,0 +1,11 @@
+# Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+# Пример:
+#- пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+def digit_factorial(num):
+    array = [1]
+    for i in range(1, num):
+        array.append(array[i-1] * (i+1)) 
+    return array
+N = int(input("Введите число N: "))
+print(f"Произведение чисел от 1 до {N} = {digit_factorial(N)}")
